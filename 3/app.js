5abd3a837ec896d2fe58aa9063a1b4a394175edc
samplex191-boop <samplex191@gmail.com>
2026-03-25 09:52:25 +0000

    final changes done.

--- 3/app.js
+++ 3/app.js
@@ -1,35 +1,4 @@
 import myModule from './myModule.js';
- 
-console.log("========================================");
-console.log("   Using User-Defined Module in Node.js ");
-console.log("========================================\n");
- 
-// 1. Using the greet function
-console.log("1. Greeting:");
-console.log(myModule.greet("Student"));
- 
-// 2. Using the add function
-console.log("\n2. Addition:");
-console.log("10 + 5 =", myModule.add(10, 5));
- 
-// 3. Using the subtract function
-console.log("\n3. Subtraction:");
-console.log("10 - 5 =", myModule.subtract(10, 5));
- 
-// 4. Using the multiply function
-console.log("\n4. Multiplication:");
-console.log("10 * 5 =", myModule.multiply(10, 5));
- 
-// 5. Using the divide function
-console.log("\n5. Division:");
-console.log("10 / 5 =", myModule.divide(10, 5));
-console.log("10 / 0 =", myModule.divide(10, 0));
- 
-// 6. Using the checkEvenOdd function
-console.log("\n6. Even or Odd:");
-console.log(myModule.checkEvenOdd(7));
-console.log(myModule.checkEvenOdd(12));
- 
-console.log("\n========================================");
-console.log("   Module operations completed!");
-console.log("========================================");
+
+console.log("Greeting:");
+console.log(myModule("Rhushikesh"));
--- 3/myModule.js
+++ 3/myModule.js
@@ -1,39 +1,4 @@
 function greet(name) {
     return "Hello, " + name + "! Welcome to Node.js Modules!";
 }
- 
-function add(a, b) {
-    return a + b;
-}
- 
-function subtract(a, b) {
-    return a - b;
-}
- 
-function multiply(a, b) {
-    return a * b;
-}
- 
-function divide(a, b) {
-    if (b === 0) {
-        return "Error: Cannot divide by zero!";
-    }
-    return a / b;
-}
- 
-function checkEvenOdd(num) {
-    if (num % 2 === 0) {
-        return num + " is Even";
-    } else {
-        return num + " is Odd";
-    }
-}
- 
-export default {
-    greet,
-    add,
-    subtract,
-    multiply,
-    divide,
-    checkEvenOdd
-};
+export default greet;
